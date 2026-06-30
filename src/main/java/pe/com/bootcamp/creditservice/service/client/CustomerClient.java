2a48845d06df1785aa1cb95056bcef5687819878
DanielJaureguiGonzales <dank_lp123@outlook.com>
2026-06-29 19:22:55 -0500

    "Agregando logback"

--- src/main/java/pe/com/bootcamp/creditservice/service/client/CustomerClient.java
+++ src/main/java/pe/com/bootcamp/creditservice/service/client/CustomerClient.java
@@ -1,24 +1,19 @@
 package pe.com.bootcamp.creditservice.service.client;
 
-import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
-import org.springframework.web.service.annotation.PostExchange;
 
 import pe.com.bootcamp.creditservice.dto.CustomerResponse;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 
 public interface CustomerClient {
 
 
     @GetExchange("/internal/customer/document-number")
     Mono<CustomerResponse> getCustomerResponse(
-            @RequestParam String documentType,
-            @RequestParam String documentNumber
+            @RequestParam String documentNumber,
+            @RequestParam String documentType
     );
 }
